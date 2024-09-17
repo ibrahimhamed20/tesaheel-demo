449c25dfa44a4731b3622064ee27f5af8962bdae
Ibrahim Hamed <ibrahimhamed2016@hotmail.com>
2024-09-17 17:20:13 +0300

    new publish 17_09_24

---
+++ chunk-NV7QS2YB.js
@@ -0,0 +1,1 @@
+import"./chunk-4CLCTAJ7.js";var t=[{path:"",loadComponent:()=>import("./chunk-KVRRHN7R.js").then(o=>o.SubscriptionsComponent),children:[{path:"subscription",loadComponent:()=>import("./chunk-OX6VFKYA.js").then(o=>o.SubscriptionComponent)},{path:"roles",loadComponent:()=>import("./chunk-OX6VFKYA.js").then(o=>o.RolesComponent)}]}];export{t as subscriptionRoutes};
